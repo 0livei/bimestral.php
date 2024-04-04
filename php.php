3117dbe35fff23bf1a54272ea7fdb5e8394a6d50
0livei <158283728+0livei@users.noreply.github.com>
2024-04-04 08:52:19 -0300

    Add files via upload
    Miguel Oliveira-Cauã menon- Leandro santos- Thiago Zamboni

---
+++ php.php
@@ -0,0 +1,37 @@
+
+<?php 
+//1
+function hellword() {
+
+
+echo 'hello world';
+}
+
+//2
+
+function name () {
+  $name  string  [leandro];
+
+echo  'hello $name '; 
+
+//3
+
+}
+
+
+ alunosname () {
+
+    $alunosname = ["miguel", "leandro", "iago", "menon", "murilo"];  
+
+    echo $alunosname [4] ;
+
+}
+
+//4
+numeros () {
+
+    $numeros = ["1", "-1", "0", "-2", "3"]; 
+
+    
+}
+?>
